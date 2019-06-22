0617b471dcef732cab6f24d11498c641e63e3ae7
Dimitres Samouel <dimitres.sam@gmail.com>
2019-06-22 20:18:05 +0100

    batch_generator.py

--- batch_generator.py
+++ batch_generator.py
@@ -13,7 +13,7 @@ class Batch_Generator():
     def num_epochs(self, inputs):
         
         batch_size = int(self.batch_size / len(inputs))
-        num_of_batches = [(int(len(inputs[i]) / batch_size) - 1) for i in range(len(inputs))]
+        num_of_batches = [(int((len(inputs[i])-self.window_size+1) / batch_size) - 1) for i in range(len(inputs))]
         n_epochs = min(num_of_batches)
         
         return n_epochs
@@ -29,23 +29,28 @@ class Batch_Generator():
 
         # Batch indexes
         self.indexes = list(range(n_epochs))
+        
         if self.shuffle == True:
             np.random.shuffle(self.indexes)
 
-        for bi, b in enumerate(self.indexes):
+        for ei, e in enumerate(self.indexes):
+            
+            offset = e * batch_size
 
             # Initialization
             X_batch = np.zeros((batch_size * num_meters, self.window_size, 1))  # (128,100,1)
             Y_batch = np.zeros((batch_size * num_meters, 1))  # (128,1)
 
-            offset = b * batch_size
-
             # Create a batch out of data from all buildings
             for i in range(num_meters):
                 mainpart = inputs[i]
                 meterpart = targets[i]
-                mainpart = mainpart[offset:offset + batch_size]
-                meterpart = meterpart[offset:offset + batch_size]
+                
+                indexer = np.arange(self.window_size)[None, :] + np.arange(len(inputs[i])-self.window_size+1)[offset:offset + batch_size, None]
+                
+                mainpart = mainpart[indexer]
+                meterpart = meterpart[self.window_size - 1:][offset:offset + batch_size]
+                
                 X = np.reshape(mainpart, (batch_size, self.window_size, 1))
                 Y = np.reshape(meterpart, (batch_size, 1))
 
@@ -55,5 +60,6 @@ class Batch_Generator():
             # Shuffle data
             p = np.random.permutation(len(X_batch))
             X_batch, Y_batch = X_batch[p], Y_batch[p]
+   
 
             yield X_batch, Y_batch
